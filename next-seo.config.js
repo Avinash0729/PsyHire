20924ea48b0e3a6a0595bffcb4794d04e9e52dca
Avinash0729 <115566017+Avinash0729@users.noreply.github.com>
2024-05-18 17:10:49 +0530

    frontend
    Frontend

---
+++ next-seo.config.js
@@ -0,0 +1,10 @@
+export default {
+  titleTemplate: "PsyHire | %s",
+  defaultTitle: "PsyHire",
+  additionalMetaTags: [
+    {
+      name: "viewport",
+      content: "width=device-width, initial-scale=1",
+    },
+  ],
+};
